3209065c373eb96c1eef1a11e0c323bccbfe1d24
r.ratsun <r.ratsun@gmail.com>
2021-11-17 15:11:54 +0200

    .

---
+++ app/Listeners/LayoutController.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ * This file is part of premium software, which is NOT free.
+ * Copyright (c) AtroCore UG (haftungsbeschränkt).
+ *
+ * This Software is the property of AtroCore UG (haftungsbeschränkt) and is
+ * protected by copyright law - it is NOT Freeware and can be used only in one
+ * project under a proprietary license, which is delivered along with this program.
+ * If not, see <https://atropim.com/eula> or <https://atrodam.com/eula>.
+ *
+ * This Software is distributed as is, with LIMITED WARRANTY AND LIABILITY.
+ * Any unauthorised use of this Software without a valid license is
+ * a violation of the License Agreement.
+ *
+ * According to the terms of the license you shall not resell, sublicense,
+ * rent, lease, distribute or otherwise transfer rights or usage of this
+ * Software or its derivatives. You may modify the code of this Software
+ * for your own needs, if source code is provided.
+ */
+
+declare(strict_types=1);
+
+namespace ExportHttp\Listeners;
+
+use Espo\Core\Utils\Json;
+use Treo\Core\EventManager\Event;
+
+class LayoutController extends \Treo\Listeners\AbstractListener
+{
+    public function afterActionRead(Event $event): void
+    {
+        $scope = $event->getArgument('params')['scope'];
+
+        $name = $event->getArgument('params')['name'];
+
+        $method = 'modify' . $scope . ucfirst($name);
+
+        if (method_exists($this, $method)) {
+            $this->{$method}($event);
+        }
+    }
+
+//    protected function modifyScheduledJobDetail(Event $event): void
+//    {
+//        $result = Json::decode($event->getArgument('result'), true);
+//
+//        $newRows = [];
+//        foreach ($result[0]['rows'] as $row) {
+//            $newRows[] = $row;
+//            if ($row[0]['name'] === 'job') {
+//                $newRows[] = [['name' => 'importFeed'], false];
+//            }
+//        }
+//
+//        $result[0]['rows'] = $newRows;
+//
+//        $event->setArgument('result', Json::encode($result));
+//    }
+}
