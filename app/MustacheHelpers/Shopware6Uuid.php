960cc6ec848a0db57ae431ab850c502773dba2e2
r.ratsun <r.ratsun@gmail.com>
2022-05-26 11:28:33 +0300

    .

--- app/MustacheHelpers/Shopware6Uuid.php
+++ app/MustacheHelpers/Shopware6Uuid.php
@@ -28,11 +28,27 @@ class Shopware6Uuid extends Injectable
 {
     public const VALID_PATTERN = '^[0-9a-f]{32}$';
 
-    public function __invoke($id)
+    public function __invoke(string $id, \Mustache_LambdaHelper $helper)
     {
-        $uuid = self::randomHex();
+        $hex = empty($id) ? bin2hex(random_bytes(16)) : self::fromStringToHex($helper->render($id));
+        $timeHi = self::applyVersion(mb_substr($hex, 12, 4), 4);
+        $clockSeqHi = self::applyVariant(hexdec(mb_substr($hex, 16, 2)));
 
-        return $uuid;
+        return sprintf(
+            '%08s%04s%04s%02s%02s%012s',
+            // time low
+            mb_substr($hex, 0, 8),
+            // time mid
+            mb_substr($hex, 8, 4),
+            // time high and version
+            str_pad(dechex($timeHi), 4, '0', \STR_PAD_LEFT),
+            // clk_seq_hi_res
+            str_pad(dechex($clockSeqHi), 2, '0', \STR_PAD_LEFT),
+            // clock_seq_low
+            mb_substr($hex, 18, 2),
+            // node
+            mb_substr($hex, 20, 12)
+        );
     }
 
     public static function randomHex(): string
