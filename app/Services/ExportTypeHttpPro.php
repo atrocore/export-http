468f360017faf6dee064a3170544b7e9e04a2f25
r.ratsun <r.ratsun@gmail.com>
2022-09-05 11:02:27 +0300

    .

--- app/Services/ExportTypeHttpPro.php
+++ app/Services/ExportTypeHttpPro.php
@@ -195,6 +195,7 @@ class ExportTypeHttpPro extends AbstractExportType
         $params = $this->getSelectParams();
         $params['offset'] = $this->data['offset'];
         $params['maxSize'] = $this->data['limit'];
+        $params['withDeleted'] = !empty($this->data['withDeleted']);
 
         $this->data['offset'] = $this->data['offset'] + $this->data['limit'];
         $this->iteration++;
