efbcc131035afb2506211cbbb262bff350e5fcf6
r.ratsun <r.ratsun@gmail.com>
2023-01-26 15:38:48 +0200

    .

--- app/Listeners/ExportFeedService.php
+++ app/Listeners/ExportFeedService.php
@@ -42,4 +42,15 @@ class ExportFeedService extends \Espo\Listeners\AbstractListener
 
         $event->setArgument('result', $result);
     }
+
+    public function prepareEntityForOutput(Event $event): void
+    {
+        $entity = $event->getArgument('entity');
+        if (!empty($entity->getFeedField('httpConnectionId'))) {
+            $connection = $this->getEntityManager()->getEntity('Connection', $entity->getFeedField('httpConnectionId'));
+            if (!empty($connection)) {
+                $entity->set('httpConnectionName', $connection->get('name'));
+            }
+        }
+    }
 }
