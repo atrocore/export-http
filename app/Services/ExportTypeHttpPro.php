7e6d37099ae1aa852656a3cfb429d5962acd288f
Roman Ratsun <rr@atrocore.com>
2023-09-13 06:42:34 +0000

    .

--- app/Services/ExportTypeHttpPro.php
+++ app/Services/ExportTypeHttpPro.php
@@ -22,7 +22,7 @@ declare(strict_types=1);
 
 namespace ExportHttp\Services;
 
-use Espo\ConnectionType\ConnectionOauth2;
+use Atro\ConnectionType\ConnectionOauth2;
 use Espo\Core\Exceptions\BadRequest;
 use Espo\Entities\Attachment;
 use Export\Entities\ExportJob;
