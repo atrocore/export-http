a473f1f60cec12cd65a2ce2ea6364fa8c8b3a209
r.ratsun <33658723+rratsun@users.noreply.github.com>
2023-11-01 14:04:58 +0200

    Update V1Dot5Dot5.php

--- app/Migrations/V1Dot5Dot5.php
+++ app/Migrations/V1Dot5Dot5.php
@@ -29,14 +29,9 @@ class V1Dot5Dot5 extends Base
 {
     public function up(): void
     {
-        /** @var \Espo\Core\Utils\Metadata $metadata */
-        $metadata = (new \Espo\Core\Application())->getContainer()->get('metadata');
-
-        if ($metadata->isModuleInstalled('Import')) {
-            $this->exec("ALTER TABLE export_feed ADD process_response_id VARCHAR(24) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`");
-            $this->exec("CREATE INDEX IDX_PROCESS_RESPONSE_ID ON export_feed (process_response_id)");
-            $this->exec("CREATE INDEX IDX_PROCESS_RESPONSE_ID_DELETE ON export_feed (process_response_id, deleted)");
-        }
+        $this->exec("ALTER TABLE export_feed ADD process_response_id VARCHAR(24) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`");
+        $this->exec("CREATE INDEX IDX_PROCESS_RESPONSE_ID ON export_feed (process_response_id)");
+        $this->exec("CREATE INDEX IDX_PROCESS_RESPONSE_ID_DELETE ON export_feed (process_response_id, deleted)");
     }
 
     public function down(): void
