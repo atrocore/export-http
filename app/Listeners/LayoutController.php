730340f6058d5c750b965edf2c32d41c06576de1
Roman Zablodskyi <rz@atrocore.com>
2023-11-01 10:32:25 +0200

    Added possibility to add ImportFeed to process export response

--- app/Listeners/LayoutController.php
+++ app/Listeners/LayoutController.php
@@ -48,6 +48,8 @@ class LayoutController extends AbstractListener
         $result[1]['rows'][] = [['name' => 'httpMethod'], ['name' => 'httpConnectionId']];
         $result[1]['rows'][] = [['name' => 'httpUrl', 'fullWidth' => true]];
 
+        $result[0]['rows'][] = [['name' => 'processResponse'], false];
+
         $event->setArgument('result', Json::encode($result));
     }
 
--- app/Services/ExportTypeHttpPro.php
+++ app/Services/ExportTypeHttpPro.php
@@ -25,13 +25,18 @@ namespace ExportHttp\Services;
 use Atro\ConnectionType\AbstractConnection;
 use Espo\Core\Exceptions\BadRequest;
 use Espo\Entities\Attachment;
+use Export\Entities\ExportFeed;
 use Export\Entities\ExportJob;
+use Import\Services\ImportFeed;
 
 class ExportTypeHttpPro extends \Export\Services\ExportTypeSimple
 {
     public function export(array $data, ExportJob $exportJob): Attachment
     {
         $attachment = parent::export($data, $exportJob);
+
+        /** @var ExportFeed $exportFeed */
+        $exportFeed = $exportJob->get('exportFeed');
 
         // save file to export job
         $exportJob->set('fileId', $attachment->get('id'));
@@ -102,6 +107,53 @@ class ExportTypeHttpPro extends \Export\Services\ExportTypeSimple
 
         if (!in_array($httpCode, [200, 201, 204])) {
             throw new BadRequest("Response Code: $httpCode Body: $output");
+        } else {
+            if (!empty($output) && !empty($importFeed = $exportFeed->get('processResponse'))) {
+                $attachmentData = new \stdClass();
+
+                $nameParts = $attachment->get('name');
+                $nameParts = explode('.', $nameParts);
+                array_pop($nameParts);
+
+                $attachmentData->name = implode('.', $nameParts);
+                $attachmentData->contents = $output;
+                $attachmentData->relatedType = 'ImportJob';
+                $attachmentData->field = 'uploadedFile';
+                $attachmentData->role = 'Attachment';
+
+                switch ($importFeed->getFeedField('format')) {
+                    case 'CSV':
+                        $attachmentData->name .= '.csv';
+                        $attachmentData->type = 'text/csv';
+                        break;
+                    case 'Excel':
+                        $attachmentData->name .= '.xlsx';
+                        $attachmentData->type = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+                        break;
+                    case 'JSON':
+                        $attachmentData->name .= '.json';
+                        $attachmentData->type = 'application/json';
+                        break;
+                    case 'XML':
+                        $attachmentData->name .= '.xml';
+                        $attachmentData->type = 'application/xml';
+                        break;
+                }
+
+                try {
+                    /** @var \Espo\Services\Attachment $attachmentService */
+                    $attachmentService = $this->getService('Attachment');
+
+                    if (!empty($attachmentImport = $attachmentService->createEntity($attachmentData))) {
+                        /** @var ImportFeed $importFeedService */
+                        $importFeedService = $this->getService('ImportFeed');
+
+                        $importFeedService->pushJobs($importFeed, $attachmentImport->id);
+                    }
+                } catch (\Throwable $e) {
+                    $GLOBALS['log']->error($e->getMessage());
+                }
+            }
         }
 
         $exportJob->set('stateMessage', $output);
