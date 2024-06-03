66209ebec08257382f20441e402e5149bb1cfd21
dzoukou <dzoukouraoul@gmail.com>
2024-06-03 14:08:17 +0100

    .

--- app/Services/ExportTypeHttpPro.php
+++ app/Services/ExportTypeHttpPro.php
@@ -26,7 +26,7 @@ namespace ExportHttp\Services;
 use Atro\ConnectionType\ConnectionHttp;
 use Atro\ConnectionType\HttpConnectionInterface;
 use Atro\Entities\File;
-use Espo\Core\Exceptions\BadRequest;
+use Atro\Core\Exceptions\BadRequest;
 use Export\Entities\ExportFeed;
 use Export\Entities\ExportJob;
 use Import\Services\ImportFeed;
@@ -79,6 +79,11 @@ class ExportTypeHttpPro extends \Export\Services\ExportTypeSimple
             $success = strtolower($res) === 'true' || $res === '1';
             if (empty($success)) {
                 throw new BadRequest("Validation failed for validator {$exportHttpValidator->get('name')}. \n Result: $res \n Response Code: $httpCode \n Body: $output");
+            }
+        } else {
+            // Standard validation
+            if ($httpCode < 200 || $httpCode >= 300) {
+                throw new BadRequest("Response Code: $httpCode Body: $output");
             }
         }
 
