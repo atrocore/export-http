6c7fa467ae7f91fb0ab27d6beaef79e2aa966d9b
Kevin Teguia <kt@atrocore.com>
2024-10-31 17:13:17 +0100

    .

--- client/modules/export-http/src/views/export-job/record/row-actions/export-again-and-remove.js
+++ client/modules/export-http/src/views/export-job/record/row-actions/export-again-and-remove.js
@@ -8,22 +8,29 @@
  * @license    GPLv3 (https://www.gnu.org/licenses/)
  */
 
-Espo.define('export-http:views/export-job/record/row-actions/export-again-and-remove', 'export:views/export-job/record/row-actions/export-again-and-remove', Dep => {
+Espo.define('export-http:views/export-job/record/row-actions/export-again-and-remove',
+    'export:views/export-job/record/row-actions/export-again-and-remove',
+    Dep => {
 
-    return Dep.extend({
+        return Dep.extend({
 
-        getActionList() {
-            let list = Dep.prototype.getActionList.call(this) || [];
+            getActionList() {
+                let list = Dep.prototype.getActionList.call(this) || [];
 
-            if (['Failed', 'Canceled'].includes(this.model.get('state')) && this.model.get('fileId') && this.model.get('requestUrl') && this.options.acl.edit) {
-                list.unshift({
-                    action: 'trySendRequestAgain', label: 'trySendRequestAgain', data: {
-                        id: this.model.id
-                    }
-                });
+                if (['Failed', 'Canceled'].includes(this.model.get('state'))
+                    && this.model.get('fileId')
+                    && this.model.get('requestUrl')
+                    && this.options.acl.edit) {
+                    list.unshift({
+                        action: 'trySendRequestAgain',
+                        label: 'trySendRequestAgain',
+                        data: {
+                            id: this.model.id
+                        }
+                    });
+                }
+
+                return list;
             }
-
-            return list;
-        }
+        });
     });
-});
