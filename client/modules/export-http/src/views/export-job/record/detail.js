dfd4e7ef8ae69bba6d8d4c148954c00ade7c1c7b
Kevin Teguia <kt@atrocore.com>
2024-10-31 17:12:41 +0100

    .

--- client/modules/export-http/src/views/export-job/record/detail.js
+++ client/modules/export-http/src/views/export-job/record/detail.js
@@ -18,7 +18,7 @@ Espo.define('export-http:views/export-job/record/detail', 'export:views/export-j
 
             Dep.prototype.setupActionItems.call(this);
 
-            if (['Failed', 'Canceled'].includes(this.model.get('state')) && && this.model.get('requestUrl') && this.model.get('fileId')) {
+            if (['Failed', 'Canceled'].includes(this.model.get('state')) && this.model.get('requestUrl') && this.model.get('fileId')) {
                 this.dropdownItemList.push({
                     name: 'trySendRequestAgain',
                     action: 'trySendRequestAgain',
